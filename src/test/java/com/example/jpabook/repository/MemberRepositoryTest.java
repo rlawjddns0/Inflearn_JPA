140261f5c6bfb713bc38268db776a0ecc5d6f725
KJW <rlawjddns0@naver.com>
2022-11-29 19:48:29 +0900

    JPA환경 설정 Member entity, MemberRepository 설정
    JPA 테스트

--- src/test/java/com/example/jpabook/repository/MemberRepositoryTest.java
+++ src/test/java/com/example/jpabook/repository/MemberRepositoryTest.java
@@ -1,7 +1,46 @@
 package com.example.jpabook.repository;
 
-import static org.junit.Assert.*;
+import com.example.jpabook.entity.Member;
+import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
-public class MemberRepositoryTest {
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    @Rollback(false)
+    public void testMember() throws Exception{
+        //given
+        Member member = new Member();
+        member.setUsername("memberA");
+
+
+        //when
+        Long save = memberRepository.save(member);
+
+
+        //then
+        Member findMember = memberRepository.find(member.getId());
+
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        assertThat(findMember).isEqualTo(member); //같다 왜?같은 트랜잭션 안에서는 영속성 컨텍스트에서 같은걸 가져온다.
+
+    }
 
 }
