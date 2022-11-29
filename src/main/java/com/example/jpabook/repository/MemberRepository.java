c0ad32770df7b3bef38853f380dc9ab7b659606e
KJW <rlawjddns0@naver.com>
2022-11-29 19:47:34 +0900

    ㅇㄹ
    ㅇㄹ

--- src/main/java/com/example/jpabook/repository/MemberRepository.java
+++ src/main/java/com/example/jpabook/repository/MemberRepository.java
@@ -1,4 +1,24 @@
 package com.example.jpabook.repository;
 
+import com.example.jpabook.entity.Member;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
 public class MemberRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+
+    public Long save(Member member){
+        em.persist(member);
+        return member.getId();
+    }
+
+    public Member find(Long id){
+        return em.find(Member.class,id);
+    }
 }
--- src/test/java/com/example/jpabook/repository/MemberRepositoryTest.java
+++ src/test/java/com/example/jpabook/repository/MemberRepositoryTest.java
@@ -1,4 +1,7 @@
+package com.example.jpabook.repository;
+
 import static org.junit.Assert.*;
+
 public class MemberRepositoryTest {
-  
+
 }
