72da671eb2abd629f4cf7a7314b81f39ab08f381
KJW <rlawjddns0@naver.com>
2022-12-29 16:17:13 +0900

    상품 수정
    변경감지와 병합 차이점 공부,
    병합(merge)는 사용x
    무조건 변경감지 이용해서 수정 해야 된다.
    병합은 모든 컬럼을 수정하며 값이 없다면 null로 수정 처리해버린다.

--- src/main/java/com/example/jpabook/controller/ItemController.java
+++ src/main/java/com/example/jpabook/controller/ItemController.java
@@ -1,13 +1,18 @@
 package com.example.jpabook.controller;
 
 import com.example.jpabook.entity.item.Book;
+import com.example.jpabook.entity.item.Item;
 import com.example.jpabook.service.ItemService;
 import lombok.RequiredArgsConstructor;
 import org.dom4j.rule.Mode;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -26,10 +31,53 @@ public class ItemController {
         Book book = new Book();
         book.setAuthor(bookForm.getAuthor());
         book.setIsbn(bookForm.getIsbn());
+        book.setName(bookForm.getName());
         book.setPrice(bookForm.getPrice());
         book.setStockQuantity(bookForm.getStockQuantity());
 
         itemService.saveItem(book);
         return "redirect:/";
+    }
+
+    @GetMapping("/items")
+    public String list(Model model){
+        List<Item> items = itemService.findItems();
+        model.addAttribute("items", items);
+        return "items/itemList";
+
+
+    }
+
+    @GetMapping("items/{itemId}/edit")
+    public String updateItemForm(@PathVariable("itemId") Long itemId, Model model){
+        Book one = (Book)itemService.findOne(itemId);
+
+        BookForm bookForm=new BookForm();
+        bookForm.setId(one.getId());
+        bookForm.setAuthor(one.getAuthor());
+        bookForm.setIsbn(one.getIsbn());
+        bookForm.setName(one.getName());
+        bookForm.setPrice(one.getPrice());
+
+        model.addAttribute("form", bookForm);
+
+        return "items/updateItemForm";
+
+
+    }
+
+    @PostMapping("items/{itemId}/edit")
+    public String updateItem(@ModelAttribute("form") BookForm form, Model model){
+        Book book=new Book();
+        book.setName(form.getName());
+        book.setId(form.getId());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setIsbn(form.getIsbn());
+        book.setAuthor(form.getAuthor());
+
+        itemService.saveItem(book);
+
+        return "redirect:/items";
     }
 }
