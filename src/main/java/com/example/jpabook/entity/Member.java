31c4d7dcbbee8f5243255e633e955f38f3f95745
KJW <rlawjddns0@naver.com>
2023-01-03 19:37:59 +0900

    수정

--- src/main/java/com/example/jpabook/entity/Member.java
+++ src/main/java/com/example/jpabook/entity/Member.java
@@ -1,6 +1,7 @@
 package com.example.jpabook.entity;
 
 import com.example.jpabook.embedded.Address;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -28,6 +29,7 @@ public class Member {
 
     //컬렉션은 필드에서 바로 초기화 하는 것이 안전하다.
     //null 문제에서 안전하다
+    @JsonIgnore //json형태로 변환 x -> api 개발시 멤버 정보만 제공해주고 싶을때
     @OneToMany(mappedBy = "member")
     private List<Order> orders = new ArrayList<>();
 
