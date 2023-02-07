cd8aefb8c42aed84428f99aeacd452ae3b58483e
KJW <rlawjddns0@naver.com>
2023-02-07 20:05:37 +0900

    페이징 가능한 페치조인 개발
    배치 설정으로 페이징 가능
    XToOne 관계는 페치조인으로 하고 나머지 관계는 지연 로딩으로 가져온다.

--- src/main/java/com/example/jpabook/api/OrderApiController.java
+++ src/main/java/com/example/jpabook/api/OrderApiController.java
@@ -11,6 +11,7 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
@@ -49,6 +50,18 @@ public class OrderApiController {
     @GetMapping("/api/v3/orders")
     public List<OrderDto> ordersV3(){
     List<Order> all = orderRepository.findAllWithItem();
+        List<OrderDto> collect = all.stream().map(o -> new OrderDto(o))
+                .collect(Collectors.toList());
+        return collect;
+
+    }
+
+    @GetMapping("/api/v3.1/orders")
+    public List<OrderDto> ordersV3_page(@RequestParam(value = "offset",defaultValue = "0") int offset,
+                                        @RequestParam(value = "limit",defaultValue = "100") int limit)
+    {
+        List<Order> all = orderRepository.findAllWithMemberDeliver(offset,limit); //XToOne 관계는 한방에 가져온다.
+
         List<OrderDto> collect = all.stream().map(o -> new OrderDto(o))
                 .collect(Collectors.toList());
         return collect;
